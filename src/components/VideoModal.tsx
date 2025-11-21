e21b901fc5732bd337cf0c6a1cb755fdac3273c5
az-badri <badrtdinoff.az27@gmail.com>
2025-11-21 20:45:50 +0300

    feat: big VideoModal created

---
+++ src/components/VideoModal.tsx
@@ -0,0 +1,54 @@
+import { Modal, Button } from 'antd';
+import ReactPlayer from 'react-player';
+import { useEffect, useState } from 'react';
+
+export const VideoModal = ({ state, send }) => {
+    const isModal = state.matches('modal');
+    const isPlaying = state.matches({ modal: 'playing' });
+
+    const [showPlayer, setShowPlayer] = useState(false);
+
+    useEffect(() => {
+        if (isModal) setShowPlayer(true);
+    }, [isModal]);
+
+    return (
+        <Modal
+            open={isModal}
+            title="Video Player"
+            onCancel={() => send({ type: 'CLOSE_MODAL' })}
+            footer={[
+                <Button key="mini" onClick={() => send({ type: 'TO_MINI' })}>
+                    Мини-плеер
+                </Button>,
+                <Button
+                    key="playpause"
+                    onClick={() => send({ type: isPlaying ? 'PAUSE' : 'PLAY' })}
+                >
+                    {isPlaying ? 'Пауза' : 'Играть'}
+                </Button>
+            ]}
+
+            width={800}
+        >
+            <div style={{ position: 'relative', paddingTop: '50%' }}>
+                {showPlayer && (
+                    <ReactPlayer
+                        url={state.context.url}
+                        playing={isPlaying}
+                        width="100%"
+                        height="100%"
+                        style={{ position: 'absolute', top: 0, left: 0 }}
+                        onBuffer={() => send({ type: 'BUFFER' })}
+                        onReady={() => send({ type: 'RESOLVE' })}
+                        config={{
+                            file: { forceHLS: true },
+                        }}
+                    />
+                )}
+            </div>
+        </Modal>
+    );
+};
+
+export default VideoModal;
